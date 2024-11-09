edd30121ca15b78a272d0563dee4902383ed74ed
Worty76 <lethanhdat762003@gmail.com>
2024-11-09 16:50:50 +0700

    feat: add body parser

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT;
 
 const userRouter = require("./src/routes/userRouter");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/user", userRouter);
 
 app.use("/", (req, res) => {
