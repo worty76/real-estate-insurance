4f7944e2857455a49068d48d297981f4d4a82497
Worty76 <lethanhdat762003@gmail.com>
2024-11-09 16:51:45 +0700

    feat(user): create

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,4 +1,4 @@
-const { sql, poolPromise } = require("../config/db");
+const { mssql, poolPromise } = require("../config/db");
 
 const read = async (req, res) => {
   try {
@@ -11,8 +11,37 @@ const read = async (req, res) => {
   }
 };
 
+const create = async (req, res) => {
+  const { userId, name, birthOfYear, phone, email, address } = req.body;
+  try {
+    const pool = await poolPromise;
+    const result = await pool
+      .request()
+      .input("userId", mssql.Int, userId)
+      .input("name", mssql.VarChar, name)
+      .input("birthOfYear", mssql.DateTime, birthOfYear)
+      .input("phone", mssql.VarChar, phone)
+      .input("email", mssql.VarChar, email)
+      .input("address", mssql.VarChar, address).query(`
+        INSERT INTO users (userId, name, birthOfYear, phone, email, address)
+        OUTPUT INSERTED.*
+        VALUES (@userId, @name, @birthOfYear, @phone, @email, @address)
+      `);
+
+    res.status(200).json({
+      message: "User created successfully",
+      user: result.recordset[0],
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error creating user", error: error.message });
+  }
+};
+
 const userControllers = {
   read,
+  create,
 };
 
 module.exports = {
