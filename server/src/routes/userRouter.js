447d27a802feaf5cbe0d55ccacac1418fdf47b66
phamhac178 <phamhac123@gmail.com>
2024-11-11 09:26:22 +0700

    feat(user): delete user

--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -5,5 +5,5 @@ const router = express.Router();
 router.get("/", userControllers.read);
 router.post("/create", userControllers.create);
 router.post("/update/:userId", userControllers.update);
-
+router.delete("/delete/:userId", userControllers.deleteUser);
 module.exports = router;
